db0668222f2382889282d529168bbb19f3a2f0a2
uriel0810 <za240111428@zapopan.tecmm.edu.mx>
2025-09-06 23:59:20 -0600

    Create resistenciatotal.py

---
+++ PYTHON/resistenciatotal.py
@@ -0,0 +1,5 @@
+a=int(input("ingresa el primer valor de la resistencia"))
+b=int(input("ingresa el segundo valor"))
+c=int(input("ingresa el tercer valor"))
+RT=a + b + c 
+print("la resistencia total sera",RT)
