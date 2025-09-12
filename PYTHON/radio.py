f12ae0f151d0dce03ef201393de0efcf12da2a72
uriel0810 <za240111428@zapopan.tecmm.edu.mx>
2025-09-12 10:30:00 -0600

    Update radio.py

--- PYTHON/radio.py
+++ PYTHON/radio.py
@@ -1,4 +1,8 @@
 import math
-a=int(input("ingresa el radio"))
-area= math.pi * a**2
-print("el area es",area)
+#a=int(input("ingresa el radio"))
+#area= math.pi * a**2
+#print("el area es",area)
+b=float(input("ingresa pi"))
+a=float(input("ingresa el radio"))
+Area=b*a**2
+print("el area de tu circulo es" ,Area )
