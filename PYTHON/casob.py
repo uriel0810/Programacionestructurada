6fa974d9ad1212954c5bc8f5335f98601315c1ba
uriel0810 <za240111428@zapopan.tecmm.edu.mx>
2025-09-12 10:30:10 -0600

    Create casob.py

---
+++ PYTHON/casob.py
@@ -0,0 +1,7 @@
+a=int(input("ingresa el numero"))
+b=int(input("ingresa el numero"))
+c=int(input("ingresa el numero"))
+d=int(input("ingresa el numero"))
+e=int(input("ingresa el numero"))
+result=a+b*c-d/e
+print("el resultado karnalito es ",result)
