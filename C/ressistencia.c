7d71569422eba20c1bbd0454f6c00f0be0a833f0
uriel0810 <za240111428@zapopan.tecmm.edu.mx>
2025-10-21 11:45:05 -0600

    Update ressistencia.c

---
+++ C/ressistencia.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+   int a;
+     int b;
+     int c;
+    int result;
+   
+ 
+ 
+    printf("ingresa el primer valor ");
+    scanf("%d",&a);
+     printf("ingresa el segundo valor ");
+    scanf("%d",&b);
+    printf("ingresa el tercer valor ");
+    scanf("%d",&c);
+    result = a + b + c ;
+   
+    printf("la resistencia total es %d\n",result);
+  
+    return 0;
+}
