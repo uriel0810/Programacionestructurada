da0e01c0064ce52dca4c82906507de4c0beb2b76
uriel0810 <za240111428@zapopan.tecmm.edu.mx>
2025-10-21 11:45:10 -0600

    Update salario.c

---
+++ C/salario.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+     float a;
+     float b;
+    float result;
+   
+ 
+ 
+    printf("ingresa el salario base ");
+    scanf("%f",&a);
+     
+    result = a * 4;
+   
+    printf("en el mes es %f\n",result);
+  
+    return 0;
+}
