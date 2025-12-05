cb376c769126c30fcb687de0f9ff123d7ce63303
uriel0810 <za240111428@zapopan.tecmm.edu.mx>
2025-12-05 11:10:29 -0600

    Update ledesp32.c

---
+++ PUERTOSERIAL/ledesp32.c
@@ -0,0 +1,16 @@
+int ledPin= 5;
+void setup()
+{
+
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop()
+{
+
+  for (int x=0; x<3; x++) {
+    digitalWrite(ledPin, HIGH);
+    delay(150);
+    digitalWrite(ledPin, LOW);
+    delay(100);
+  }
